628b2908d294347cc87327e16eeb56cf83e76f49
Noah Platton <noahplatton@gmail.com>
2022-02-22 13:29:26 +0000

    Set up route to get all users

--- client/js/handlers.js
+++
@@ -1,8 +0,0 @@
-const { requestLogin, requestRegistration } = require('./auth')
-
-const loginForm = document.getElementById('login-form');
-const registerForm = document.getElementById('register-form');
-
-loginForm.addEventListener('submit', requestLogin)
-registerForm.addEventListener('submit', requestRegistration)
-
--- client/main.js
+++ client/main.js
@@ -1,23 +1,35 @@
+const { requestLogin, requestRegistration } = require("./auth/auth");
+
+const loginForm = document.getElementById("login-form");
+const registerForm = document.getElementById("register-form");
+
+loginForm.addEventListener("submit", requestLogin);
+registerForm.addEventListener("submit", requestRegistration);
+
+// ---------------- ORG PAGE -----------------------
+
 const container = document.querySelector("#inner-container");
 
 let x0;
-container.addEventListener("mousedown", (e) => {
-  x0 = e.clientX;
-  let x1;
-  container.addEventListener("mouseup", (e) => {
-    x1 = e.clientX;
-    slider(x0, x1);
+container &&
+  container.addEventListener("mousedown", (e) => {
+    x0 = e.clientX;
+    let x1;
+    container.addEventListener("mouseup", (e) => {
+      x1 = e.clientX;
+      slider(x0, x1);
+    });
   });
-});
 
-container.addEventListener("touchstart", (e) => {
-  let x0 = e.touches.item(0).clientX;
-  let x1;
-  container.addEventListener("touchend", (e) => {
-    x1 = e.changedTouches.item(0).clientX;
-    slider(x0, x1);
+container &&
+  container.addEventListener("touchstart", (e) => {
+    let x0 = e.touches.item(0).clientX;
+    let x1;
+    container.addEventListener("touchend", (e) => {
+      x1 = e.changedTouches.item(0).clientX;
+      slider(x0, x1);
+    });
   });
-});
 
 function slider(x0, x1) {
   const i = getComputedStyle(document.documentElement).getPropertyValue("--i");
@@ -28,5 +40,3 @@ function slider(x0, x1) {
     document.documentElement.style.setProperty("--i", 0);
   }
 }
-
-require("./js/auth");
